5d5ee870ef07270ea10acba7ec57a83ae042b5c4
Dennis Musembi <dennismusembi344@gmail.com>
2023-03-14 20:20:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+let activity="roller-skate"
+function saturdayFun (activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(`This Saturday, I want to ${activity}!`)
+saturdayFun("bathe my dog")
+
+
+function mondayWork (activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+console.log(`This Monday, I will ${activity}.`)
+mondayWork("work from home")
+
+
+
+
+
+function wrapAdjective(star="*"){
+    return function result(adjective){
+    return `You are ${star}${adjective}${star}!`
+}
+}
+console.log(`You are${star} ${adjective} ${star}`)
+wrapAdjective(star="||")
+result(adjective="a dedicated programmer")
+console.log(`You are ${star}${adjective} ${star}`)
+
